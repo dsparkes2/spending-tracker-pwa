dcd44cffaf17088dfa55c25f992f1fd6a9e1e782
dsparkes2 <dcsparkes2@gmail.com>
2026-08-11 19:34:05 -0400

    Add POST route for user registration

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const controller = require('../controllers/authController');
 router.get('/login', controller.loginPage);
 router.get('/register', controller.registerPage);
+router.post('/register', controller.register);
 module.exports = router;
