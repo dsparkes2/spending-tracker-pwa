3d3598ac456d89107d769ac49fa43de23f066a5e
dsparkes2 <dcsparkes2@gmail.com>
2026-08-11 19:57:03 -0400

    Implement user registration with hashed passwords
    Added user registration functionality with password hashing.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,37 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
-exports.loginPage = (req, res) => res.render('login');
-exports.registerPage = (req, res) => res.render('register');
+exports.loginPage = (req, res) => {
+    res.render('login');
+};
+
+exports.registerPage = (req, res) => {
+    res.render('register');
+};
+
+exports.register = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const existingUser = await User.findOne({ username });
+
+        if (existingUser) {
+            return res.send('Username already exists. Please choose another username.');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            username,
+            password: hashedPassword
+        });
+
+        await user.save();
+
+        res.redirect('/auth/login');
+
+    } catch (error) {
+        console.error('Registration error:', error);
+        res.status(500).send('Something went wrong while creating your account.');
+    }
+};
