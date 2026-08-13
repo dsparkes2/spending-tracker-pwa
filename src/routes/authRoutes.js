f80e8a33d36aabb9d899a3034796c6ffe68dd8b2
dsparkes2 <dcsparkes2@gmail.com>
2026-08-12 22:37:44 -0400

    Add session test route to authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,4 +6,10 @@ router.get('/login', controller.loginPage);
 router.post('/login', controller.login);
 router.get('/register', controller.registerPage);
 router.post('/register', controller.register);
+router.get('/session-test', (req, res) => {
+    res.json({
+        loggedIn: !!req.session.userId,
+        userId: req.session.userId || null
+    });
+});
 module.exports = router;
