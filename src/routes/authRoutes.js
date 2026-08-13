da9335d20da5d449aab71f926ed0bbf7f2ea2806
dsparkes2 <dcsparkes2@gmail.com>
2026-08-13 05:49:04 -0400

    Remove session test route from authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,5 @@ router.get('/login', controller.loginPage);
 router.post('/login', controller.login);
 router.get('/register', controller.registerPage);
 router.post('/register', controller.register);
-router.get('/session-test', (req, res) => {
-    res.json({
-        loggedIn: !!req.session.userId,
-        userId: req.session.userId || null
-    });
-});
+
 module.exports = router;
