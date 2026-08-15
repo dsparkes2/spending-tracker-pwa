cfa6b3bd8fc5d5ae26f266ba07824b2f8c1fdd10
dsparkes2 <dcsparkes2@gmail.com>
2026-08-15 08:26:47 -0400

    Filter expenses by user household

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,8 +1,20 @@
 const Expense = require("../models/Expense");
-
+const User = require("../models/User");
 exports.getExpenses = async (req, res) => {
   try {
-    const expenses = await Expense.find()
+    const user = await User.findById(req.session.userId);
+
+    if (!user) {
+      return res.redirect("/auth/login");
+    }
+
+    if (!user.household) {
+      return res.send("Your account is not connected to a household yet.");
+    }
+
+    const expenses = await Expense.find({
+      household: user.household
+    })
       .populate("user")
       .populate("household")
       .populate("budget")
