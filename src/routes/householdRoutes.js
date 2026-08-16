a9fc54e60dbd61f25ed63abae93ba7db8a64c58d
dsparkes2 <dcsparkes2@gmail.com>
2026-08-16 08:31:47 -0400

    Add household setup route

--- src/routes/householdRoutes.js
+++ src/routes/householdRoutes.js
@@ -1,1 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/householdController");
+const authMiddleware = require("../middleware/authMiddleware");
 
+router.get("/setup", authMiddleware, controller.setupPage);
+
+module.exports = router;
