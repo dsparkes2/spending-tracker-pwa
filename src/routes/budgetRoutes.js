c943ca4c211f074d8e681e7bdafd2a99f8486681
dsparkes2 <dcsparkes2@gmail.com>
2026-08-17 20:53:24 -0400

    Add route for new budget page

--- src/routes/budgetRoutes.js
+++ src/routes/budgetRoutes.js
@@ -5,5 +5,5 @@ const controller = require('../controllers/budgetController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', authMiddleware, controller.getBudget);
-
+router.get('/new', authMiddleware, controller.newBudgetPage);
 module.exports = router;
