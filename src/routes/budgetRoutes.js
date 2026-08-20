3cfe563adc779fb0b377d400d86a741ffc698b27
dsparkes2 <dcsparkes2@gmail.com>
2026-08-19 20:56:49 -0400

    Add route to update budget

--- src/routes/budgetRoutes.js
+++ src/routes/budgetRoutes.js
@@ -9,4 +9,5 @@ router.get('/new', authMiddleware, controller.newBudgetPage);
 router.get('/categories', authMiddleware, controller.budgetCategoriesPage);
 router.post('/categories', authMiddleware, controller.saveBudgetCategories);
 router.get('/edit', authMiddleware, controller.editBudgetPage);
+router.post('/edit', authMiddleware, controller.updateBudget);
 module.exports = router;
